7a9027d70037bbefffd1f2cac069553d133e282a
Yairsep <yairsep@gmail.com>
2020-09-14 12:13:51 +0300

    Commit 14.9 - Routing

---
+++ client/src/components/Team.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Image from "react-bootstrap/Image";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
+function Team() {
+    const path = "../data/team.jpeg"
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1 style={{ textAlign: "center" }}>Team</h1>
+        <img src='team.png' class="center" height="70%" width="70%"/>
+    </div>
+  );
+}
+
+export default Team;
---
+++ client/src/firebaseConfig.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyB5DCwhhCnyHh5x9yGnuiirTQH6mt1rKac",
+  authDomain: "bgu-hack-27983.firebaseapp.com",
+  databaseURL: "https://bgu-hack-27983.firebaseio.com",
+  projectId: "bgu-hack-27983",
+  storageBucket: "bgu-hack-27983.appspot.com",
+  messagingSenderId: "307735563620",
+  appId: "1:307735563620:web:4a5248c9f33b9391f8fe71",
+  measurementId: "G-YMRHHXWESP",
+};
