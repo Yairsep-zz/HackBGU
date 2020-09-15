2d968acfa811952f3a762ef929c80c8fc4a3d150
Yairsep <yairsep@gmail.com>
2020-09-15 11:01:24 +0300

    Commit 15.9 - Auth feature

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -5,7 +5,7 @@ function About() {
     <div>
       <h1 style={{ textAlign: "center" }}>אודות</h1>
         <br/>
-        <div style={{textAlign: "center", alignItems: "center" , justifyContent: "center"}}>
+        <div style={{marginLeft: "200px" ,marginRight: "200px", textAlign: "center", alignItems: "center" , justifyContent: "center"}}>
             <h2>
                 נעים מאוד, אנו ProBonoz באים לסייע לקהל הסטודנטים בתהליך חיפוש אחר עבודה בייחוד בימים אלו בהם אנו חווים את הקשיים הנלווים למשבר הקורונה
             </h2>
